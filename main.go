2bf38d07149819c9c3c01117127809da2700df6d
alvaroenriqueds <alvaroenrique020@gmail.com>
2019-06-17 18:27:22 -0500

    Se agrega los middleware y el puerto

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+        "github.com/labstack/echo"
+        "github.com/labstack/echo/middleware"
+)
+
 func main()  {
-        
+        e := echo.New()
+
+        e.Use(middleware.Logger())
+        e.Use(middleware.Recover())
+        e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+                AllowOrigins:   []string{"*"},
+                AllowMethods:   []string{echo.GET, echo.POST},
+        }))
+
+
+        e.Start(":5050")
 }
